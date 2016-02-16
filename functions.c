994907f85a3803950b692b4a4975a511bed4d0c2
Rutvij Bhatt <bhatt.rut@gmail.com>
2016-02-15 19:32:55 -0500

    fixed makefile

--- functions.c
+++ functions.c
@@ -3,20 +3,19 @@
 
 #include <string.h>
 
-static int l_split(lua_State *L)
-{
-    const char *str = luaL_checkstring(L, 1); // first argument
-    const char *sep = luaL_checkstring(L, 2); // second argument
+static int l_split(lua_State *L) {
+    const char *str = luaL_checkstring(L, 1);  // first argument
+    const char *sep = luaL_checkstring(L, 2);  // second argument
 
     const char *e;
     int i = 1;
 
-    lua_newtable(L); // result table
+    lua_newtable(L);  // result table
 
     while ((e = strchr(str, *sep)) != NULL) {
-	lua_pushlstring(L, str, e-str); // push substring
-	lua_rawseti(L, -2, i++);
-	str = e+ 1; // advance string's current pos past separator
+        lua_pushlstring(L, str, e - str);  // push substring
+        lua_rawseti(L, -2, i++);
+        str = e + 1;  // advance string's current pos past separator
     }
 
     lua_pushstring(L, str);
@@ -25,10 +24,7 @@ static int l_split(lua_State *L)
     return 1;
 }
 
-const struct luaL_reg mylib[] = {
-    {"split", l_split},
-    {NULL, NULL}
-};
+const struct luaL_reg mylib[] = {{"split", l_split}, {NULL, NULL}};
 
 int luaopen_mylib(lua_State *L) {
     luaL_openlib(L, "mylib", mylib, 0);
